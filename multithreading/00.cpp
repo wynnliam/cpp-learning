51baba82a7ceeaff032bd7ddb6b36fe03ce1a6ad
Liam Wynn <wynnliam@pdx.edu>
2020-06-20 08:36:02 -0700

    Added multithreading demo

---
+++ multithreading/00.cpp
@@ -0,0 +1,34 @@
+// Liam Wynn, 6/20/2020, C++ Learning
+
+/*
+	A tiny introduction to multithreading in C++
+
+	To compile:
+	g++ -lpthread 00.cpp
+*/
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void function_1() {
+	cout << "Hello from function 1" << endl;
+}
+
+int main() {
+	std::thread t1(function_1); // Starts t1
+	//t1.join(); // Main thread waits for t1 to finish.
+	t1.detach(); // t1 will run freely on its own. -- t1 is now a daemon process
+	// Doing this will typically cause no output.
+	// main thread runs too fast for t1 to print.
+	//while(true); // Uncomment this as well as detach to see t1 still print
+
+	//t1.join(); // Once you detach, you cannot rejoin. This gives error
+
+	// You can test joinability though like so
+	if(t1.joinable())
+		t1.join();
+
+	return 0;
+}
